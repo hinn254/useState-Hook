79eee739558aea7739a8b77bd01b9a7306ebff91
hinn254 <bennyhinnotieno@gmail.com>
2021-10-11 22:07:57 +0300

    usestate tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("Ian");
+  const [greeting, setGreeting] = useState("Hi");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {greeting} {name} Welcome
+      </h1>
+      <button onClick={() => setName("Benny")}>CHANGE NAME</button>
+      <button onClick={() => setGreeting("Hello")}>CHANGE GREETING</button>
     </div>
   );
 }
